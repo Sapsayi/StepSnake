d40eef7bd18face3edb284b3cbf6c3335803d67c
Sapsayi <vitaliisapsay@gmail.com>
2025-04-10 22:48:34 +0300

    Update hot reload

--- Assets/StepSnake/Scripts/Consumables/Consumable.cs
+++ Assets/StepSnake/Scripts/Consumables/Consumable.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public abstract class Consumable : MonoBehaviour
@@ -5,4 +7,9 @@ public abstract class Consumable : MonoBehaviour
     public abstract void Activate(Snake snake);
 
     public Vector2Int Pos => new((int)transform.position.x, (int)transform.position.y);
+
+    private void Start()
+    {
+        transform.DOScale(1f, 0.2f).From(0f).SetEase(Ease.OutBack);
+    }
 }
